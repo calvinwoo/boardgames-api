f62a7630808c14310dcee2325bc1fc3d2e0a40d8
Calvin Woo <calvinwoo32@gmail.com>
2017-01-29 02:00:34 -0800

    Create recommend API
    - In memory array of games
    - Recommends games by same category

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const Koa = require('koa');
+const json = require('koa-json');
+const routes = require('./src/routes');
 
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  const start = new Date();
-  const ms = new Date() - start;
-  await next();
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
-
-app.use(ctx => {
-  ctx.body = 'Hello Koa@2';
-});
-
-app.listen(7000);
+app
+  .use(json())
+  .use(routes)
+  .listen(7000);
---
+++ src/boardgames/index.js
@@ -0,0 +1,34 @@
+const boardgames = [
+  {
+    id: '1',
+    name: 'Puerto Rico',
+    category: 'euro',
+    rating: 8.1,
+  },
+  {
+    id: '2',
+    name: 'San Juan',
+    category: 'euro',
+    rating: 7.3,
+  },
+  {
+    id: '3',
+    name: 'Cards Against Humanity',
+    category: 'party',
+    rating: 6.2,
+  },
+  {
+    id: '3',
+    name: 'Agricola',
+    category: 'euro',
+    rating: 8.1,
+  },
+  {
+    id: '4',
+    name: 'Dominion',
+    category: 'strategy',
+    rating: 7.7,
+  }
+];
+
+module.exports = boardgames;
