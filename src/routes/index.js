70d6e2bf1815947d33c3671844b24c307e80b180
jzlung <jzlung@gmail.com>
2017-02-06 23:30:39 -0800

    updated top 100 game data with statistics, fixed router to have simple algo

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,12 @@ router
     const count = ctx.query.count || 1
 
     const baseGame = boardgames.find(boardgame => boardgame['@objectid'] === ctx.params.baseGameId) || {}
-    console.log(baseGame['@objectid'])
+
     ctx.body = boardgames
-      .filter(boardgame => boardgame === baseGame)
-//      .filter(boardgame => boardgame.boardgamedesigner === baseGame.boardgamedesigner)
-//      .sort((game1, game2) => game1.rating - game2.rating)
+      .filter(boardgame => boardgame['@objectid'] !== baseGame['@objectid'])
+      .filter(boardgame => boardgame.minplayers === baseGame.minplayers)
+      .filter(boardgame => boardgame.maxplayers === baseGame.maxplayers)
+      .sort((game1, game2) => game1.statistics.ratings.ranks['@id'] - game2.statistics.ratings.ranks['@id'])
       .slice(0, count)
   })
 
