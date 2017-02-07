983e3c2c25e9f541862567727fa12615bd5f1220
jzlung <jzlung@gmail.com>
2017-02-06 21:07:32 -0800

    added data for top 100

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # boardgames-api
+
+How the API currently works:
+There is a list of board games, similar to the one in your code, that's stored in memory.
+https://github.com/calvinwoo/boardgames-api/blob/master/src/boardgames/index.js
+Each board game contains an additional ID, which is just an arbitrary number.
+
+There is one routes, which responds with a list of recommended games, based on one that is provided. Currently, it just finds any other games with the same category. You can also provide an optional "count" query parameter, which sets a limit to the number games to respond with. The default value of count is 1.
+
+Here is an example call: https://boardgames-api.herokuapp.com/recommend/base-game/1?count=2
+
+And here is the code: https://github.com/calvinwoo/boardgames-api/blob/master/src/routes/index.js
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,13 @@ const router = new Router()
 router
   .get('/recommend/base-game/:baseGameId', async ctx => {
     const count = ctx.query.count || 1
-
-    const baseGame = boardgames.find(boardgame => boardgame.id === ctx.params.baseGameId) || {}
-
+    
+    const baseGame = boardgames.find(boardgame => boardgame['@objectid'] === ctx.params.baseGameId) || {}
+    console.log(baseGame['@objectid']);
     ctx.body = boardgames
-      .filter(boardgame => boardgame.id !== baseGame.id)
-      .filter(boardgame => boardgame.category === baseGame.category)
-      .sort((game1, game2) => game1.rating - game2.rating)
+      .filter(boardgame => boardgame === baseGame)
+//      .filter(boardgame => boardgame.boardgamedesigner === baseGame.boardgamedesigner)
+//      .sort((game1, game2) => game1.rating - game2.rating)
       .slice(0, count)
   })
 
