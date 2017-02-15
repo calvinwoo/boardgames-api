4395037c7698ea006f0a75a1449a6c04413e4896
Calvin Woo <calvinwoo32@gmail.com>
2017-02-14 18:01:21 -0800

    Test DB connection

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const Koa = require('koa')
 const json = require('koa-json')
 const cors = require('koa-cors')
+const pg = require('pg')
 const routes = require('./src/routes')
+
+pg.defaults.ssl = !!process.env.DATABASE_URL
+pg.connect(process.env.DATABASE_URL || 'postgres://localhost:5432/bigboardofgames', (err, client) => {
+  if (err) {
+    throw err
+  }
+
+  console.log('Connected to postgres! Getting schemas...')
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', (row) => {
+      console.log(JSON.stringify(row))
+    })
+})
 
 const app = new Koa()
 const options = {
