a7f5050c92f80198054b23fc919868c2a9418e43
Calvin Woo <calvinwoo32@gmail.com>
2017-02-14 22:15:23 -0800

    Add endpoint for publishing articles

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
 const json = require('koa-json')
+var bodyParser = require('koa-bodyparser')
 const cors = require('koa-cors')
 const routes = require('./src/routes')
 
@@ -9,5 +10,6 @@ app.use(cors({credentials: true}))
 
 app
   .use(json())
+  .use(bodyParser())
   .use(routes)
   .listen(process.env.PORT || 3000)
